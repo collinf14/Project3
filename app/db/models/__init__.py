c8b90b85b234f924f565bad02c95b0988fe9a4cb
Collin Fogerty <clf9@njit.edu>
2022-04-28 19:47:08 -0400

    changes to db model

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -1,16 +1,25 @@
 from datetime import datetime
 
 from sqlalchemy import Integer, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, declarative_base
 from werkzeug.security import check_password_hash, generate_password_hash
 from app.db import db
 from flask_login import UserMixin
 from sqlalchemy_serializer import SerializerMixin
 
-#location_user = db.Table
-#song_user = db.Table
+Base = declarative_base()
 
-class Song(db.Model,SerializerMixin):
+location_user = db.Table('location_user', db.Model.metadata,
+                         db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+                         db.Column('location_id', db.Integer, db.ForeignKey('locations.id'))
+                         )
+song_user = db.Table('song_user', db.Model.metadata,
+                     db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+                     db.Column('song_id', db.Integer, db.ForeignKey('songs.id'))
+                     )
+
+
+class Song(db.Model, SerializerMixin):
     __tablename__ = 'songs'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(300), nullable=True, unique=False)
@@ -34,10 +43,10 @@ class Song(db.Model,SerializerMixin):
             'genre': self.genre,
         }
 
+
 class Location(db.Model, SerializerMixin):
     __tablename__ = 'locations'
     serialize_only = ('title', 'longitude', 'latitude')
-
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(300), nullable=True, unique=False)
@@ -74,16 +83,17 @@ class User(UserMixin, db.Model):
     is_admin = db.Column('is_admin', db.Boolean(), nullable=False, server_default='0')
     songs = db.relationship("Song", back_populates="user", cascade="all, delete")
     locations = db.relationship("Location", back_populates="user", cascade="all, delete")
-    #locations = db.relationship("Location", secondary=location_user, backref="users")
-    #songs = db.relationship("Song", secondary=song_user, backref="users")
+    #locations = db.relationship("Location",secondary=location_user, backref="users")
+    #songs = db.relationship("Song",secondary=song_user, backref="users")
 
     # `roles` and `groups` are reserved words that *must* be defined
     # on the `User` model to use group- or role-based authorization.
 
-    def __init__(self, email, password):
+    def __init__(self, email, password, is_admin):
         self.email = email
         self.password = password
         self.registered_on = datetime.utcnow()
+        self.is_admin = is_admin
 
     def is_authenticated(self):
         return True
@@ -105,5 +115,3 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.email
-
-
